6f18e9df52fe89fb70f3c3a5022dfdc2059ba68f
Deven Shah <devenshah1974@gmail.com>
2024-05-22 17:31:45 +0100

    Message class can be in diff project but has to have same namespace

---
+++ src/NS.SB.Functions.Handlers/Messages/HelloWorldEvent.cs
@@ -0,0 +1,5 @@
+﻿namespace NS.SB.Functions.Messages;
+public record HelloWorldEvent(string Name, DateTime MessageTime) : IEvent
+{
+    public string Message => $"Hello {Name}! on {MessageTime.ToShortTimeString()}";
+}
